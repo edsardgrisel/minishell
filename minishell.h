5f37859a6e88a09a108858f5d0e6010f3c49f827
Edsard <egrisel.student@codam.nl>
2025-09-03 16:47:26 +0200

    create ast_node_type

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: egrisel <egrisel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/25 15:54:02 by egrisel           #+#    #+#             */
-/*   Updated: 2025/08/29 14:20:07 by egrisel          ###   ########.fr       */
+/*   Updated: 2025/09/03 16:47:00 by egrisel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 /// @brief NONE acts as a null terminator for t_token lists. so if list[i].type
 /// is NONE, it indicates that the previous token was the last element
-enum e_token_type
+typedef enum e_token_type
 {
 	NONE = 0,
 	WORD,
@@ -30,7 +30,7 @@ enum e_token_type
 	REDIRECT_HEREDOC,
 	REDIRECT_APPEND,
 	UNSUPPORTED
-};
+}	t_token_type;
 
 typedef struct s_open_quote
 {
@@ -40,7 +40,7 @@ typedef struct s_open_quote
 
 typedef struct s_token
 {
-	enum e_token_type	type;
+	t_token_type		type;
 	char				*value;
 }	t_token;
 
@@ -58,12 +58,23 @@ typedef struct s_alloced_list
 	struct s_alloced_list	*next;
 }	t_alloced_list;
 
-typedef struct s_ast
+typedef enum e_ast_node_type
 {
+	COMMAND,
+	PIPE,
+	REDIRECT_IN,
+	REDIRECT_OUT,
+	REDIRECT_HEREDOC,
+	REDIRECT_APPEND,
+}	t_ast_node_type;
+
+typedef struct s_ast_node
+{
+	t_ast_node_type	node_type;
 	struct s_ast	*left;
 	struct s_ast	*right;
 
-}	t_ast;
+}	t_ast_node;
 
 t_token	*tokenize(char *str);
 void	free_double_pointer(void **ptr);
